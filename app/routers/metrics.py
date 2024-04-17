bdeb649c4ccd3d294d97ed9bf79e7c05d7012ae6
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2024-04-17 13:32:06 -0500

    Add formater and style tools

--- app/routers/metrics.py
+++ app/routers/metrics.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Query
 
 router = APIRouter(
     prefix="/metrics",
@@ -7,11 +8,26 @@ router = APIRouter(
 )
 
 
+# TODO: This must be a post request
 @router.get("/{metric_id}/areas")
-async def get_areas():
-    return [{"Perdida": 2035}, {"Persistencia": 40843}, {"No bosque": 207122}]
+async def get_areas(
+    metric_id: str,
+    polygon: Annotated[str | None, Query(max_length=1)] = None,
+):
+    return [
+        {"Perdida": 2035},
+        {"Persistencia": 40843},
+        {"No bosque": 207122},
+    ]
+
+
+# TODO: This must be a post request
 
 
 @router.get("/{metric_id}/layer")
-async def get_layer(metric_id):
+async def get_layer(metric_id: str):
     return {"layer": "format to be defined"}
+
+
+# Validaciones del polígono
+# Deshabilitar el tryout en modo producción del swagger

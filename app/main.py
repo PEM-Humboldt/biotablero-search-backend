d5ed6dc550bcede5c191b7c9f111586cb873cb8f
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2024-04-17 18:03:34 -0500

    Add more endpoints and improve documentation

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 
 from .routers import metrics
 
-app = FastAPI()
+
+app = FastAPI(
+    title="BioTableroSearch",
+    description="Get metrics by predefined or custom (polygon) areas.",
+    summary="Metrics for BioTablero Search module",
+    version="0.1.0",
+    contact={
+        "name": "Equipo BioTablero",
+        "url": "http://biotablero.humboldt.org.co/",
+        "email": "biotablero@humboldt.org.co",
+    },
+)
 
 
 app.include_router(metrics.router)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+# TODO: disable swagger on production: https://fastapi.tiangolo.com/tutorial/metadata/#docs-urls
--- app/routers/metrics.py
+++ app/routers/metrics.py
@@ -1,33 +1,87 @@
-from typing import Annotated
-from fastapi import APIRouter, Query
+from typing import Annotated, Literal
+import fastapi
+from pydantic import BaseModel, Field
 
-router = APIRouter(
+router = fastapi.APIRouter(
     prefix="/metrics",
     tags=["metrics"],
     responses={404: {"description": "Not found"}},
 )
 
 
-# TODO: This must be a post request
-@router.get("/{metric_id}/areas")
-async def get_areas(
-    metric_id: str,
-    polygon: Annotated[str | None, Query(max_length=1)] = None,
+async def metric_id_param(
+    metric_id: Annotated[
+        Literal["LossPersistence", "Coverage"],
+        fastapi.Path(description="metric you whish to query"),
+    ]
 ):
+    return {"metric_id": metric_id}
+
+
+async def defined_areas_params(
+    area_type: Annotated[
+        str,
+        fastapi.Query(description="type of the predefined area", example="ea"),
+    ],
+    area_id: Annotated[
+        str, fastapi.Query(description="id of the area", example="CAR")
+    ],
+):
+    return {"area_type": area_type, "area_id": area_id}
+
+
+class Polygon(BaseModel):
+    polygon: str = Field(
+        description="GeoJSON polygon to determine the query area"
+        # TODO: Add input example
+    )
+
+
+# TODO: Successful response examples for /areas
+# TODO: 422 response examples for /areas and /layer
+
+
+@router.get("/{metric_id}/areas")
+async def get_areas_by_defined_area(
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    defined_area: Annotated[dict, fastapi.Depends(defined_areas_params)],
+) -> list[dict[str, float]]:
+    """
+    Given a metric and a predefined area of interest, get the area values for each category in the metric inside the indicated area
+    """
+    return {"response": "WIP"}
+
+
+@router.post("/{metric_id}/areas")
+async def get_areas_by_polygon(
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    polygon: Polygon,
+):
+    """
+    Given a metric and a polygon, get the area values for each category in the metric inside the polygon
+    """
     return [
-        {"Perdida": 2035},
-        {"Persistencia": 40843},
-        {"No bosque": 207122},
+        {"Perdida": 2035, "Persistencia": 40843, "No bosque": 207122},
     ]
 
 
-# TODO: This must be a post request
-
-
 @router.get("/{metric_id}/layer")
-async def get_layer(metric_id: str):
-    return {"layer": "format to be defined"}
+async def get_layer_by_defined_area(
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    defined_area: Annotated[dict, fastapi.Depends(defined_areas_params)],
+):
+    """
+    Given a metric and a predefined area of interest, get the layer of the metric vut by the indicated area
+    """
+    return {"layer": "response to be defined"}
 
 
-# Validaciones del polígono
-# Deshabilitar el tryout en modo producción del swagger
+@router.post("/{metric_id}/layer")
+async def get_layer_by_polygon(
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    polygon: Polygon,
+):
+    """
+    Given a metric and a predefined area of interest, get the layer of the metric vut by the indicated area
+    """
+    return {"layer": "response to be defined"}

0f90b2e7e3d26ee7d7dd7c26ce947d35f40c051b
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2024-04-17 20:01:16 -0500

    Read dotenv and disable swagger in production

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+from functools import lru_cache
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .routers import metrics
 
 
+class Settings(BaseSettings):
+    stac_url: str = "http://localhost:8080"
+    env: str = "dev"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
 app = FastAPI(
     title="BioTableroSearch",
     description="Get metrics by predefined or custom (polygon) areas.",
@@ -13,6 +28,7 @@ app = FastAPI(
         "url": "http://biotablero.humboldt.org.co/",
         "email": "biotablero@humboldt.org.co",
     },
+    docs_url=None if settings.env.lower() == "prod" else "/docs",
 )
 
 

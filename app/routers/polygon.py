c085421cbb72ac40ee8a758c0234b318e7d6fba0
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-05-06 12:50:22 -0500

    Separate polygon class and add example

---
+++ app/routers/polygon.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from geojson_pydantic import Feature
+
+coordinates = [
+    [-78.990935, 12.437303],
+    [-78.990935, 4.214931],
+    [-66.851117, 4.214931],
+    [-66.851117, 12.437303],
+    [-78.990935, 12.437303]
+]
+
+geojson_polygon = {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [coordinates]
+    }
+}
+
+
+class Polygon(BaseModel):
+    polygon: Feature = Field(
+        description="GeoJSON polygon to determine the query area",
+        example=geojson_polygon
+    )

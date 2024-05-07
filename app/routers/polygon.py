75adc42fd45dc43d4e05aab591630cebef97f3bd
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-05-07 14:30:21 -0500

    Format with black

--- app/routers/polygon.py
+++ app/routers/polygon.py
@@ -6,21 +6,18 @@ coordinates = [
     [-78.990935, 4.214931],
     [-66.851117, 4.214931],
     [-66.851117, 12.437303],
-    [-78.990935, 12.437303]
+    [-78.990935, 12.437303],
 ]
 
 geojson_polygon = {
     "type": "Feature",
     "properties": {},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [coordinates]
-    }
+    "geometry": {"type": "Polygon", "coordinates": [coordinates]},
 }
 
 
 class Polygon(BaseModel):
     polygon: Feature = Field(
         description="GeoJSON polygon to determine the query area",
-        example=geojson_polygon
+        example=geojson_polygon,
     )

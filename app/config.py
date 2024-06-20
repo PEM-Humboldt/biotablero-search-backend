1ef30bb760d87ff11782ec9f5fd55a5fbbad29e4
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-06-20 18:17:30 -0500

    Add logs config

--- app/config.py
+++ app/config.py
@@ -1,12 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+from logging import basicConfig, INFO
 
 
 class Settings(BaseSettings):
-    stac_url: str = "http://localhost:8080"
+
+    stac_url: str = "http://localhost:8082"
     env: str = "dev"
 
     model_config = SettingsConfigDict(env_file=".env")
+
+    def configure_logging(self):
+        basicConfig(
+            level=INFO,
+            filename="app/logs/app.log",
+            format="%(asctime)s.%(msecs)03d %(levelname)s - %(name)s - %(module)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
 
 
 @lru_cache

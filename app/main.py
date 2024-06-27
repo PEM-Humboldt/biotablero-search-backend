a2ef981d04dd5439b9e112d2361a1e29f32dc462
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-06-27 17:40:24 -0500

    Register logs for HTTP and validation exceptions

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 
 from app.routers import metrics
 from app.config import get_settings
+from logging import getLogger
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 settings = get_settings()
 settings.configure_logging()
+logger = getLogger(__name__)
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
 
 app = FastAPI(
     title="BioTableroSearch",
@@ -18,6 +29,19 @@ app = FastAPI(
     },
     docs_url=None if settings.env.lower() == "prod" else "/docs",
 )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    logger.error(f"HTTP Exception: {exc}")
+    return JSONResponse(exc.detail, status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    logger.error(f"Validation Error: {exc}")
+    return JSONResponse(exc)
+
 
 app.include_router(metrics.router)
 # TODO: disable swagger on production: https://fastapi.tiangolo.com/tutorial/metadata/#docs-urls

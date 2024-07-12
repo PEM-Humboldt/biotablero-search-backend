a56509901fb09a752ac93eeade7aa3814de119bc
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-07-12 16:13:44 -0500

    Use contextvar for manage request_id value

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ from app.config import get_settings
 from logging import getLogger
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from uuid import uuid4
+from app.services.utils import context_vars
 
 settings = get_settings()
 settings.configure_logging()
 logger = getLogger(__name__)
-
+request_id_context = context_vars.request_id_context
 
 app = fastapi.FastAPI(
     title="BioTableroSearch",
@@ -28,13 +29,17 @@ app = fastapi.FastAPI(
 @app.middleware("http")
 async def log_requests(request: fastapi.Request, call_next):
     request_id = str(uuid4())
+    request_id_context.set(request_id)
+
     logger.info(
-        f"Request ID: {request_id} - Method: {request.method} - URL: {request.url}"
+        f"Method: {request.method} - URL: {request.url}",
+        extra={"request_id": request_id},
     )
 
     response = await call_next(request)
     logger.info(
-        f"Request ID: {request_id} - Response status: {response.status_code }"
+        f"Response status: {response.status_code }",
+        extra={"request_id": request_id},
     )
     return response
 
@@ -42,7 +47,8 @@ async def log_requests(request: fastapi.Request, call_next):
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     logger.error(
-        f"HTTP Exception: {exc} - Path: {request.url} - Method: {request.method}"
+        f"HTTP Exception: {exc} - Path: {request.url} - Method: {request.method}",
+        extra={"request_id": request_id_context.get()},
     )
     return fastapi.responses.JSONResponse(
         exc.detail, status_code=exc.status_code
@@ -52,7 +58,8 @@ async def http_exception_handler(request, exc):
 @app.exception_handler(fastapi.exceptions.RequestValidationError)
 async def validation_exception_handler(request, exc):
     logger.error(
-        f"Validation error: {exc.errors()} - Path: {request.url} - Method: {request.method}"
+        f"Validation error: {exc.errors()} - Path: {request.url} - Method: {request.method}",
+        extra={"request_id": request_id_context.get()},
     )
     return fastapi.responses.JSONResponse(
         status_code=422,

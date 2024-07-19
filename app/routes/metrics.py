5482de57341e73be62c7b98b69c9f034d9e2cd9e
Denis Armenta <darmenta@humboldt.org.co>
2024-07-19 15:33:17 -0500

    Validation Setting for BBOX
    Validation Setting for BBOX

--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -1,18 +1,9 @@
-import typing
-import fastapi
-from fastapi import Depends, Body, HTTPException
-from geojson_pydantic import Feature, Polygon
-from pydantic import BaseModel, Field
-from geojson_pydantic import Feature
+from typing import Annotated, Literal, List
 
-<<<<<<< Updated upstream
-from app.routes.schemas.polygon import (
-    PolygonRequest,
-    FeatureRequest,
-    WrappedFeatureRequest,
-)
-=======
->>>>>>> Stashed changes
+import fastapi
+from pydantic import BaseModel, Field
+
+from app.routes.schemas.polygon import Polygon
 from app.services.metrics import Metrics as metrics_service
 from logging import getLogger
 from app.services.utils import context_vars
@@ -53,32 +44,30 @@ router = fastapi.APIRouter(
 
 
 async def metric_id_param(
-    metric_id: typing.Annotated[
-        typing.Literal["LossPersistence", "Coverage"],
-        fastapi.Path(description="Metric you wish to query"),
+    metric_id: Annotated[
+        Literal["LossPersistence", "Coverage"],
+        fastapi.Path(description="metric you whish to query"),
     ]
-) -> str:
-    return metric_id
+):
+    return {"metric_id": metric_id}
 
 
 async def defined_areas_params(
-    area_type: typing.Annotated[
+    area_type: Annotated[
         str,
         fastapi.Query(description="type of the predefined area", example="ea"),
     ],
-    area_id: typing.Annotated[
+    area_id: Annotated[
         str, fastapi.Query(description="id of the area", example="CAR")
     ],
 ):
     return {"area_type": area_type, "area_id": area_id}
 
 
-@router.get("/{metric_id}/areas", response_model=typing.List[AreasResponse])
+@router.get("/{metric_id}/areas", response_model=List[AreasResponse])
 async def get_areas_by_defined_area(
-    metric_id: typing.Annotated[str, fastapi.Depends(metric_id_param)],
-    defined_area: typing.Annotated[
-        dict, fastapi.Depends(defined_areas_params)
-    ],
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    defined_area: Annotated[dict, fastapi.Depends(defined_areas_params)],
 ) -> list[dict[str, float]]:
     """
     Given a metric and a predefined area of interest, get the area values for each category in the metric inside the indicated area
@@ -90,68 +79,27 @@ async def get_areas_by_defined_area(
     ]
 
 
-@router.post("/{metric_id}/areas", response_model=typing.List[AreasResponse])
+@router.post("/{metric_id}/areas", response_model=List[AreasResponse])
 async def get_areas_by_polygon(
-<<<<<<< Updated upstream
-    metric_id: str,
-    polygon: typing.Union[FeatureRequest, WrappedFeatureRequest],
-    metric_id_param: str = fastapi.Depends(metric_id_param),
-) -> typing.List[AreasResponse]:
-=======
-    metric_id: Annotated[str, Depends(metric_id_param)],
-    feature: Annotated[Feature, Body(...)],
-) -> List[AreasResponse]:
->>>>>>> Stashed changes
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    polygon: Polygon,
+) -> list[dict[str, float]]:
     """
     Given a metric and a polygon, get the area values for each category in the metric inside the polygon
     """
     try:
-<<<<<<< Updated upstream
-        if isinstance(polygon, WrappedFeatureRequest):
-            geometry = polygon.polygon.geometry
-        else:
-            geometry = polygon.geometry
-
-        # Validación adicional si se desea
-        if geometry.bbox is None:
-            raise ValueError("Bounding box (bbox) is required in geometry.")
-
-        # Lógica para obtener áreas basadas en el polígono
-        return [
-            {"key": "Perdida", "value": 2035},
-            {"key": "Persistencia", "value": 40843},
-            {"key": "No bosque", "value": 207122},
-        ]
-
-    except ValueError as ve:
-        raise fastapi.HTTPException(status_code=400, detail=str(ve))
-=======
-        print(feature)
-        geometry = feature.geometry  # Accede a la geometría del Feature
-
         data = metrics_service.get_areas_by_polygon(
-            geometry.dict()  # Usa la geometría del Feature
+            polygon.polygon.model_dump()
         )
         return data
-    except ValueError as ve:
-        raise HTTPException(status_code=400, detail=f"Invalid input: {ve}")
-    except ConnectionError as ce:
-        raise HTTPException(
-            status_code=503, detail=f"Service unavailable: {ce}"
-        )
->>>>>>> Stashed changes
     except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Internal server error: {e}"
-        )
+        raise fastapi.HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/{metric_id}/layer")
 async def get_layer_by_defined_area(
-    metric_id: typing.Annotated[str, fastapi.Depends(metric_id_param)],
-    defined_area: typing.Annotated[
-        dict, fastapi.Depends(defined_areas_params)
-    ],
+    metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
+    defined_area: Annotated[dict, fastapi.Depends(defined_areas_params)],
 ):  # TODO: Define return type
     """
     Given a metric and a predefined area of interest, get the layer of the metric cut by the indicated area
@@ -159,59 +107,20 @@ async def get_layer_by_defined_area(
     return {"layer": "response to be defined"}
 
 
-@router.post(
-    "/{metric_id}/areas/defined", response_model=typing.List[AreasResponse]
-)
-async def get_areas_by_predefined_polygon(
-    metric_id: str,
-    polygon: typing.Union[FeatureRequest, WrappedFeatureRequest],
-    metric_id_param: str = fastapi.Depends(metric_id_param),
-) -> typing.List[AreasResponse]:
-    """
-    Given a metric and a predefined polygon, get the area values for each category in the metric inside the polygon
-    """
-    try:
-        # Validación del bbox utilizando la función importada
-        if polygon.geometry.bbox is None:
-            raise ValueError("Bounding box (bbox) is required in geometry.")
-
-        # Lógica para obtener áreas basadas en el polígono
-        data = metrics_service.get_areas_by_polygon(polygon.model_dump())
-        return data
-    except ValueError as ve:
-        raise fastapi.HTTPException(status_code=400, detail=str(ve))
-    except Exception as e:
-        raise fastapi.HTTPException(status_code=500, detail=str(e))
-
-
 @router.post("/{metric_id}/layer")
 async def get_layer_by_polygon(
-<<<<<<< Updated upstream
-    metric_id: typing.Annotated[str, fastapi.Depends(metric_id_param)],
-    polygon: FeatureRequest,
-=======
     metric_id: Annotated[str, fastapi.Depends(metric_id_param)],
-    feature: Annotated[Feature, Body(...)],
->>>>>>> Stashed changes
+    polygon: Polygon,
 ):
     """
     Given a metric and a predefined area of interest, get the layer of the metric cut by the indicated area
     """
     logger = getLogger(__name__)
     try:
-        logger.info(f"Processing request for metric_id: {metric_id}")
-        geometry = feature.geometry
-
         raster_bytes = metrics_service.get_layer_by_polygon(
-<<<<<<< Updated upstream
-            metric_id, polygon.model_dump()
-=======
-            metric_id["metric_id"], geometry.dict()
->>>>>>> Stashed changes
+            metric_id["metric_id"], polygon.polygon.model_dump()
         )
-
         return fastapi.Response(content=raster_bytes, media_type="image/png")
-
     except Exception as e:
         logger.error(
             f"Execution error: {e}",
--- app/routes/schemas/polygon.py
+++ app/routes/schemas/polygon.py
@@ -1,130 +1,68 @@
-<<<<<<< Updated upstream
 from pydantic import BaseModel, Field, field_validator
-from geojson_pydantic import Feature
-from typing import List, Optional
-=======
-from pydantic import BaseModel, Field
-from geojson_pydantic.geometries import Polygon
->>>>>>> Stashed changes
+from geojson_pydantic import Feature, geometries, types
+from typing import Union, Tuple
 
-from app.services.utils import context_vars
+coordinates = [
+    [-74.66791948382932, 3.9616641000901702],
+    [-74.54883485657162, 3.6278270139496063],
+    [-73.91938754106665, 3.8258815720871353],
+    [-74.3787139604892, 4.312330649691575],
+    [-74.66791948382932, 3.9616641000901702],
+]
+
+bbox = [
+    -74.66791948382932,
+    3.6278270139496063,
+    -73.91938754106665,
+    4.312330649691575,
+]
+
+geojson_polygon = {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "Polygon",
+        "bbox": bbox,
+        "coordinates": [coordinates],
+    },
+}
 
 
-<<<<<<< Updated upstream
-class PolygonRequest(BaseModel):
-    type: str
-    coordinates: List[List[List[float]]]
-    bbox: Optional[List[float]] = None
+class PolygonGeometry(geometries.Polygon):
+    bbox: types.BBox
 
-    @field_validator("bbox")
+    @field_validator("bbox", mode="before")
     def validate_bbox(cls, v):
-        if v is not None:
-            if len(v) not in [4, 6]:
+        if len(v) not in [4, 6]:
+            raise ValueError("Bounding box (bbox) must have 4 or 6 elements.")
+        min_lon, min_lat, max_lon, max_lat = v[:4]
+        if not (-180 <= min_lon <= 180) or not (-180 <= max_lon <= 180):
+            raise ValueError("Longitude values must be between -180 and 180.")
+        if not (-90 <= min_lat <= 90) or not (-90 <= max_lat <= 90):
+            raise ValueError("Latitude values must be between -90 and 90.")
+        if min_lon > max_lon:
+            raise ValueError(
+                "Minimum longitude cannot be greater than maximum longitude."
+            )
+        if min_lat > max_lat:
+            raise ValueError(
+                "Minimum latitude cannot be greater than maximum latitude."
+            )
+        if len(v) == 6:
+            min_alt, max_alt = v[4], v[5]
+            if min_alt > max_alt:
                 raise ValueError(
-                    "Bounding box (bbox) must have 4 o 6 elements."
+                    "Minimum altitude cannot be greater than maximum altitude."
                 )
-            min_lon, min_lat, max_lon, max_lat = v[:4]
-            if not (-180 <= min_lon <= 180) or not (-180 <= max_lon <= 180):
-                raise ValueError(
-                    "Longitude values must be between -180 and 180."
-                )
-            if not (-90 <= min_lat <= 90) or not (-90 <= max_lat <= 90):
-                raise ValueError("Latitude values must be between -90 y 90.")
-            if min_lon > max_lon:
-                raise ValueError(
-                    "Minimum longitude cannot be greater than maximum longitude."
-                )
-            if min_lat > max_lat:
-                raise ValueError(
-                    "Minimum latitude cannot be greater than maximum latitude."
-                )
-            if len(v) == 6:
-                min_alt, max_alt = v[4], v[5]
-                if min_alt > max_alt:
-                    raise ValueError(
-                        "Minimum altitude cannot be greater than maximum altitude."
-                    )
         return v
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [102.0, 2.0],
-                        [103.0, 2.0],
-                        [103.0, 3.0],
-                        [102.0, 3.0],
-                        [102.0, 2.0],
-                    ]
-                ],
-                "bbox": [102.0, 2.0, 103.0, 3.0],
-            }
-        }
+
+class PolygonFeature(Feature):
+    geometry: Union[PolygonGeometry, None]
 
 
-class FeatureRequest(BaseModel):
-    type: str
-    properties: dict
-    geometry: PolygonRequest
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [
-                        [
-                            [102.0, 2.0],
-                            [103.0, 2.0],
-                            [103.0, 3.0],
-                            [102.0, 3.0],
-                            [102.0, 2.0],
-                        ]
-                    ],
-                    "bbox": [102.0, 2.0, 103.0, 3.0],
-                },
-            }
-        }
-
-
-class WrappedFeatureRequest(BaseModel):
-    polygon: FeatureRequest
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "polygon": {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "type": "Polygon",
-                        "bbox": [
-                            -76.2114265687811,
-                            4.66464238363919,
-                            -75.3755113620596,
-                        ],
-                        "coordinates": [
-                            [
-                                [-76.008254260126, 5.56368303184647],
-                                [-76.0024992672274, 5.56019970305395],
-                                [-76.0022908115716, 5.55269347472411],
-                                [-75.9989734130925, 5.54151494575816],
-                            ]
-                        ],
-                    },
-                }
-            }
-        }
-=======
-class FeatureWithPolygon(BaseModel):
-    type: str
-    properties: dict
-    geometry: Polygon = Field(
+class Polygon(BaseModel):
+    polygon: PolygonFeature = Field(
         description="GeoJSON polygon to determine the query area",
         example=geojson_polygon,
     )
->>>>>>> Stashed changes

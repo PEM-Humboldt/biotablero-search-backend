763a19ac8309d894dd65360ba6a64f2bda4ff63e
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2024-08-14 09:28:54 -0500

    Refactor collection util to get all assets url for a collection

--- app/services/utils/collection.py
+++ app/services/utils/collection.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+import typing
 import requests
 from fastapi import HTTPException
 
@@ -7,9 +7,9 @@ from app.utils import config
 settings = config.get_settings()
 
 
-def get_collection_items_url(collection_id: str) -> Optional[str]:
+def get_collection_items_url(collection_id: str) -> typing.Optional[str]:
     response = None
-    collection_url = f"{settings.stac_url}{collection_id}"
+    collection_url = f"{settings.stac_url}/collections/{collection_id}"
 
     try:
         response = requests.get(collection_url)
@@ -34,26 +34,14 @@ def get_collection_items_url(collection_id: str) -> Optional[str]:
             detail=f"No collection data found at URL: {collection_url}",
         )
 
-    items_url = f"{collection_url}/items"
-
-    try:
-        response = requests.get(items_url)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        if response.status_code == 404:
-            raise HTTPException(
-                status_code=404, detail=f"Items not found at URL: {items_url}"
-            )
-        else:
-            raise HTTPException(
-                status_code=response.status_code,
-                detail=f"HTTP error occurred while accessing {items_url}",
-            )
-
-    return items_url
+    return f"{collection_url}/items"
 
 
-def load_first_item_asset(items_url: str) -> Optional[Dict[str, Any]]:
+def get_items_asset_url(
+    collection_id: str,
+) -> typing.Dict[str, typing.Any]:
+    items_url = get_collection_items_url(collection_id)
+
     response = None
     try:
         response = requests.get(items_url)
@@ -70,45 +58,27 @@ def load_first_item_asset(items_url: str) -> Optional[Dict[str, Any]]:
             )
 
     items_data = response.json()
-
     if not items_data.get("features"):
         raise HTTPException(
             status_code=404,
             detail=f"No features found in the items data at URL: {items_url}",
         )
 
-    first_item = items_data["features"][0]
-    assets = first_item.get("assets", {})
+    def get_asset_url(item):
+        assets = item.get("assets", {})
+        if not assets:
+            return None
+        primary_asset = assets.get(item["id"])
+        if not primary_asset:
+            primary_asset = list(assets.values())[0]
+        asset_url = primary_asset.get("href")
+        return asset_url
 
-    if not assets:
-        raise HTTPException(
-            status_code=404,
-            detail=f"No assets found in the first item at URL: {items_url}",
+    assets_urls = dict(
+        map(
+            lambda item: (item["id"], get_asset_url(item)),
+            items_data.get("features"),
         )
+    )
 
-    first_asset = list(assets.values())[0]
-
-    if not first_asset:
-        raise HTTPException(
-            status_code=404,
-            detail=f"No valid asset found in the first item at URL: {items_url}",
-        )
-
-    return first_asset
-
-
-def get_first_item_asset_from_collection(
-    collection_id: str,
-) -> Optional[Dict[str, Any]]:
-    items_url = get_collection_items_url(collection_id)
-
-    first_asset = load_first_item_asset(items_url)
-
-    value_href = first_asset.get("href")
-
-    if not value_href:
-        raise HTTPException(
-            status_code=404, detail="No 'href' found in the first asset."
-        )
-
-    return value_href
+    return {k: v for k, v in assets_urls.items() if v is not None}

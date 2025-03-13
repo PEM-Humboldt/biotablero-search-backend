db7d39db89300bde52f368ed6a593e9859c4afe8
Denis Armenta <darmenta@humboldt.org.co>
2025-03-12 21:36:55 -0500

    adjust format
    adjust format

--- database/post_migrate.py
+++ database/post_migrate.py
@@ -1,4 +1,3 @@
-
 from database.populate_db import populate_db
 import logging
 from app.utils.config import get_settings
@@ -12,7 +11,9 @@ logger = logging.getLogger(__name__)
 async def post_migrate():
     logger.info("Ejecutando post-migrate...", extra={"request_id": "N/A"})
     await populate_db()
-    logger.info("Se ejecutaron los inserts correctamente", extra={"request_id": "N/A"})
+    logger.info(
+        "Se ejecutaron los inserts correctamente", extra={"request_id": "N/A"}
+    )
 
 
 if __name__ == "__main__":

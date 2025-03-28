4a1a95e01ae596174d0bd3ce72583a29fcf15304
cgalvis <cgalvis@humboldt.org.co>
2025-03-28 17:08:59 -0500

    feat: insert additional polygons working

--- database/insert_polygons.py
+++ database/insert_polygons.py
@@ -15,6 +15,8 @@ settings.configure_logging()
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_UNKNOWN_VALUE = "Desconocido"
+
 
 async def get_area_type(area_name: str):
     try:
@@ -29,21 +31,31 @@ def generate_hash(geometry):
     ).hexdigest()
 
 
-async def insert_polygons_from_geojson(file_path):
+async def insert_states_from_geojson(area_type, file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    # Add states polygons
-    area_type_obj = await get_area_type("states")
+    area_type_obj = await get_area_type(area_type)
+
+    polygon_name = "name"
+    # states
+    if area_type == "states":
+        polygon_name = "dpto_cnmbr"
+    # sa ()
+    elif area_type == "sa":
+        polygon_name = "nombre"
+    # basinSubzones
+    elif area_type == "basinSubzones":
+        polygon_name = "nom_szh"
 
     polygons = []
     for feature in data["features"]:
         geometry = feature.get("geometry", None)
         if not geometry:
             continue
-
+        
         area = feature["properties"].get("shape_Area", 0)
-        name = feature["properties"].get("dpto_cnmbr", "Desconocido")
+        name = feature["properties"].get(polygon_name, DEFAULT_UNKNOWN_VALUE)
 
         polygon = Polygon(
             hash=generate_hash(str(geometry)),
@@ -74,7 +86,9 @@ async def insert_polygons(polygons):
 async def run():
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas(safe=True)
-    await insert_polygons_from_geojson("data/departamentos.geojson")
+    await insert_states_from_geojson("states", "data/departamentos.geojson")
+    await insert_states_from_geojson("ea", "data/jurisdicciones-ambientales.geojson")
+    await insert_states_from_geojson("basinSubzones", "data/subzonas-hidrograficas.geojson")
     await Tortoise.close_connections()
 
 

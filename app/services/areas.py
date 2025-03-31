412590c498754a7d28485d363210c91b9b7e9a40
cgalvis <cgalvis@humboldt.org.co>
2025-03-31 12:34:56 -0500

    feat: added area type validation in areas endpoint

--- app/services/areas.py
+++ app/services/areas.py
@@ -8,12 +8,14 @@ from tortoise import Tortoise
 async def get_areas_by_type(area_type_id: str) -> List[AreaResponse]:
     await Tortoise.init(config=TORTOISE_ORM)
 
+    areas = []
     area_type = await AreaType.get_or_none(id=area_type_id)
 
-    area_db_dict = await Polygon.filter(area_type=area_type).values(
-        "id", "name"
-    )
-    areas = [AreaResponse(**area) for area in area_db_dict]
+    if area_type != None:
+        area_db_dict = await Polygon.filter(area_type=area_type).values(
+            "id", "name"
+        )
+        areas = [AreaResponse(**area) for area in area_db_dict]
 
     await Tortoise.close_connections()
 

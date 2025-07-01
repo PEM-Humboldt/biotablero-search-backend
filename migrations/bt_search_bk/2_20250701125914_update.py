a684045b661cecfe271aef3b392229e40f917cbb
cgalvis <cgalvis@humboldt.org.co>
2025-07-01 13:01:38 -0500

    feat: added polygon official code with unique values

---
+++ migrations/bt_search_bk/2_20250701125914_update.py
@@ -0,0 +1,11 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE UNIQUE INDEX "uid_polygon_officia_b324a3" ON "polygon" ("official_code");"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        DROP INDEX "idx_polygon_officia_b324a3";"""

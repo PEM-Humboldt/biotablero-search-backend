a28f198f0b04e8b0d35074a42ba3f8161e5c3e52
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2026-08-14 17:15:47 -0500

    Organize and connect raster functions with the rest of the endpoint

--- app/routes/schemas/LayerResponse.py
+++ app/routes/schemas/LayerResponse.py
@@ -12,3 +12,25 @@ class LayerResponse(BaseModel):
         json_schema_extra = {
             "example": {"layer": "http://localhost:4556/eads345.../layer"}
         }
+
+
+class LayerWithBboxResponse(BaseModel):
+    """
+    Response model used to return the URL and bbox of a generated image layer for visualization
+    """
+
+    layer: str
+    bbox: tuple[int, int, int, int]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "layer": "http://localhost:4556/eads345.../layer",
+                "bbox": [
+                    -79.00757596899996,
+                    -4.228886820867842,
+                    -66.84332596899996,
+                    12.458363179132157,
+                ],
+            }
+        }

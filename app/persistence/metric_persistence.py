3176be866dbb3fb71f6e749880211892d508caeb
Camilo Zapata <cazm013@gmail.com>
2026-08-18 17:22:48 -0500

    Create get_metric_by_id method to obtain metric having an specific id

--- app/persistence/metric_persistence.py
+++ app/persistence/metric_persistence.py
@@ -10,3 +10,14 @@ async def get_metric_by_name(
     return await Metric.get_or_none(name=metric_name).prefetch_related(
         "collections", "indicator"
     )
+
+
+async def get_metric_by_id(
+    metric_id: int,
+) -> Metric | None:
+    """
+    Get Metric object by id.
+    """
+    return await Metric.get_or_none(id=metric_id).prefetch_related(
+        "collections", "indicator", "info"
+    )

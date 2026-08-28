ab653a4cfe2c60b37030e4fda5be1fb4bc08346c
Camilo Zapata <cazm013@gmail.com>
2026-08-28 17:23:27 -0500

    Add doctring to get_metric_info_by_metric to function

--- app/persistence/metric_info_persistence.py
+++ app/persistence/metric_info_persistence.py
@@ -1,5 +1,6 @@
+from typing import List
 from app.models.models import MetricInfo
 
-
-async def get_metric_info_by_metric(metric_name: str):
+async def get_metric_info_by_metric(metric_name: str) -> List[MetricInfo]:
+    """Return all MetricInfo records associated with a metric."""
     return await MetricInfo.filter(metric__name=metric_name)

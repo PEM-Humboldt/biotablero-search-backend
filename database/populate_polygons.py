7a8a111f1e77ded723e0ad2433785bd9c2caec74
Jorge Higuita <jluis.higuita@gmail.com>
2026-08-28 09:16:00 -0500

    Add dict and list object manage

--- database/populate_polygons.py
+++ database/populate_polygons.py
@@ -38,14 +38,12 @@ NAMES_QUERY = """
     ORDER BY name;
 """
 
-
 def get_ids(conn):
     with conn.cursor() as cur:
         cur.execute(IDS_QUERY)
 
         for row in cur:
             yield row[0]
-
 
 def get_names(conn):
     with conn.cursor() as cur:
@@ -54,13 +52,13 @@ def get_names(conn):
         for row in cur:
             yield row[0]
 
-
 def call_api_values(polygon_id, metric_id):
     url = GET_VALUES_POLYGON.format(
         polygon_id=polygon_id,
         metric_id=metric_id,
     )
-
+    print(url) #PARA PRUEBAS, SE BORRA LUEGO
+    
     response = requests.get(
         url,
         headers={
@@ -78,7 +76,8 @@ def call_api_layer(polygon_id, metric_id, item_id, class_id):
         item_id=item_id,
         class_id=class_id
     )
-
+    print(url) #PARA PRUEBAS, SE BORRA LUEGO
+    
     response = requests.get(
         url,
         headers={
@@ -93,96 +92,88 @@ def call_api_layer(polygon_id, metric_id, item_id, class_id):
 def main():
 
     with psycopg.connect(**DB_CONFIG) as conn:
-
-        # Guardamos los names porque los vamos a reutilizar
         names = list(get_names(conn))
-
         print(f"Names encontrados: {len(names)}")
-
         for polygon_id in get_ids(conn):
-
             print(f"\n=== ID: {polygon_id} ===")
-
             for metric_id in names:
-
                 print(
                     f"Consultando "
                     f"id={polygon_id}, "
                     f"name={metric_id}"
                 )
-
                 try:
-
                     response = call_api_values(
                         polygon_id,
                         metric_id,
                     )
-
                     print(
                         f"  Status: {response.status_code}"
                     )
-
                     if response.ok:
                         print(
                             f"  OK: {response.text}"
                         )
-                        data = response.json()
-                         
-                        try: 
-                            item_id = data["id"]
+                        if metric_id in ["dpc", "statsOnSpecies", "currentHF_average", "currenrRecordsGaps_average"]:
+                            continue
+                        else:    
+                            data = response.json()
                             
-                            response_keys = [ key for key in data if key != "id" ]
-                            
-                            for key in response_keys:
-                                class_id = key
+                            if isinstance(data, dict):
+                                objects = [data]
+                            elif isinstance(data, list):
+                                objects = data
+                            else:
+                                objects = []
+                            try:
+                                for obj in objects:
+                                    item_id = obj.get("id")
+                                    for key, value in obj.items():
+                                        if key == "id":
+                                            continue
+                                        class_id = key
+                                        if metric_id in ["recordGaps", "richness"]:
+                                            class_id = metric_id
+                                        print(
+                                            f"    item_id : {item_id}, "
+                                            f"class_id : {class_id}"
+                                        )
+                                        try:#Poblado de polygon_metric_item
+                                            response = call_api_layer(
+                                                polygon_id,
+                                                metric_id,
+                                                item_id,
+                                                class_id
+                                            )
+                                            print(
+                                                f"  Status: {response.status_code}"
+                                            )
+                                            if response.ok:
+                                                print(
+                                                    f"  OK: {response.text}"
+                                                )
+                                            else:
+                                                print(
+                                                    f"  ERROR: {response.text}"
+                                                )
+                                        except requests.RequestException as exc:
+                                            print(
+                                                f"  ERROR de conexión: {exc}"
+                                            )
+                                        time.sleep(0.2)                            
+                            except KeyError as ke:
                                 print(
-                                    f"    item_id : {item_id}, "
-                                    f"class_id : {class_id}"
+                                    f"  ERROR: No hay item_id"
                                 )
-                                try:
-                                    response = call_api_layer(
-                                        polygon_id,
-                                        metric_id,
-                                        item_id,
-                                        class_id
-                                    )
-
-                                    print(
-                                        f"  Status: {response.status_code}"
-                                    )
-
-                                    if response.ok:
-                                        print(
-                                            f"  OK: {response.text}"
-                                        )
-                                    else:
-                                        print(
-                                            f"  ERROR: {response.text}"
-                                        )
-
-                                except requests.RequestException as exc:
-
-                                    print(
-                                        f"  ERROR de conexión: {exc}"
-                                    )
-                                time.sleep(1.0)                            
-                        except KeyError as ke:
-                            print(
-                                f"  ERROR: No hay item_id"
-                            )
                     else:
                         print(
                             f"  ERROR: {response.text}"
                         )
-
                 except requests.RequestException as exc:
-
                     print(
                         f"  ERROR de conexión: {exc}"
                     )
-
-                # Esperar un poco antes de la siguiente petición
-                time.sleep(1)
+                time.sleep(0.2)
 
 
 if __name__ == "__main__":

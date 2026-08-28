36de30a7f2735f6203666802fc006a64720d0ffd
Paola Andrea Montoya Lopera <amontoya@humboldt.org.co>
2026-08-28 07:50:26 -0500

    function name changed

--- app/services/metrics.py
+++ app/services/metrics.py
@@ -566,7 +566,7 @@ async def calculate_frequency_values(
     }
 
 
-async def calculate_frequency_selected_values(
+async def calculate_frequency_values_selected_coll(
     metric: Metric, polygon_obj: Polygon, group: str | None = None
 ) -> Dict[str, str | list[float] | list[int]]:
     """
@@ -798,7 +798,7 @@ class OperationFunctions:
             "SELECTED-TABLE_PRECALCULATED": calculate_table_precalculated_values,
             "FREQUENCY_SINGLE-COLLECTION": calculate_frequency_values,
             "FREQUENCY_SINGLE-SELECTED-COLLECTION": (
-                calculate_frequency_selected_values
+                calculate_frequency_values_selected_coll
             ),
         }
         layer_functions = {

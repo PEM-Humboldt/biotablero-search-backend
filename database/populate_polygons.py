c9e9264ce55d7b51af35b42cd39a527f6097483e
Jorge Higuita <jluis.higuita@gmail.com>
2026-09-21 08:11:22 -0500

    Changes at logic

--- database/populate_polygons.py
+++ database/populate_polygons.py
@@ -1,7 +1,7 @@
 import psycopg
 import requests
 
-
+API_BASE_URL = "http://127.0.0.1:8000"
 # =========================
 # PostgreSQL
 # =========================
@@ -18,40 +18,34 @@ DB_CONFIG = {
 # API
 # =========================
 
-GET_VALUES_POLYGON = "http://127.0.0.1:8000/metrics/{metric_id}/values/{polygon_id}"
-GET_LAYER_POLYGON = "http://127.0.0.1:8000/metrics/{metric_id}/layer?polygon_id={polygon_id}&item_id={item_id}&class_id={class_id}"
+GET_METRIC_GROUPS = "{api_base}/metrics/{metric_id}/groups"
+GET_VALUES_POLYGON = (
+    "{api_base}/metrics/{metric_id}/values/{polygon_id}?group={group}"
+)
+GET_LAYER_POLYGON = "{api_base}/metrics/{metric_id}/layer?polygon_id={polygon_id}&item_id={item_id}&class_id={class_id}&group={group}"
 
 # =========================
 # Consultas
 # =========================
+QUERY = "SELECT {select_columns} FROM {from_table} ORDER BY {order_column}"
 
-POLYGONS_QUERY = """
-    SELECT id
-    FROM polygon
-    ORDER BY id;
-"""
 
-METRICS_QUERY = """
-    SELECT name
-    FROM metric
-    ORDER BY name;
-"""
-
-def get_polygons(conn):
+def get_query(conn, select_columns, from_table, order_column):
     with conn.cursor() as cur:
-        cur.execute(POLYGONS_QUERY)
+        cur.execute(
+            QUERY.format(
+                select_columns=select_columns,
+                from_table=from_table,
+                order_column=order_column,
+            )
+        )
         for row in cur:
             yield row[0]
 
-def get_metrics(conn):
-    with conn.cursor() as cur:
-        cur.execute(METRICS_QUERY)
-        for row in cur:
-            yield row[0]
 
-def call_api_values(polygon_id, metric_id):
-    url = GET_VALUES_POLYGON.format(
-        polygon_id=polygon_id,
+def call_api_groups(api_base, metric_id):
+    url = GET_METRIC_GROUPS.format(
+        api_base=api_base,
         metric_id=metric_id,
     )
     log_result(f"  URL: {url}")
@@ -63,12 +57,32 @@ def call_api_values(polygon_id, metric_id):
     )
     return response
 
-def call_api_layer(polygon_id, metric_id, item_id, class_id):
+
+def call_api_values(api_base, polygon_id, metric_id, group):
+    url = GET_VALUES_POLYGON.format(
+        api_base=api_base,
+        polygon_id=polygon_id,
+        metric_id=metric_id,
+        group=group,
+    )
+    log_result(f"  URL: {url}")
+    response = requests.get(
+        url,
+        headers={
+            "accept": "application/json",
+        },
+    )
+    return response
+
+
+def call_api_layer(api_base, polygon_id, metric_id, item_id, class_id, group):
     url = GET_LAYER_POLYGON.format(
+        api_base=api_base,
         polygon_id=polygon_id,
         metric_id=metric_id,
         item_id=item_id,
-        class_id=class_id
+        class_id=class_id,
+        group=group,
     )
     log_result(f"  URL: {url}")
     response = requests.get(
@@ -78,92 +92,134 @@ def call_api_layer(polygon_id, metric_id, item_id, class_id):
         },
     )
     return response
+
 
 def log_result(text):
     print(text)
     with open("./logs/populate_polygons_res.txt", "a", encoding="utf-8") as f:
         f.write(text + "\n")
 
+
 def main():
     print("Proceso Iniciado.")
     with psycopg.connect(**DB_CONFIG) as conn:
-        metric = list(get_metrics(conn))
-        log_result(f"metric encontradas: {len(metric)}")
-        for polygon_id in get_polygons(conn):
-            log_result(f"\n=== ID: {polygon_id} ===")
-            for metric_id in metric:
-                #Si alguna metrica es de las siguientes o el id del poligono es menor a 2, se omite la consulta de polygon_metric
-                if (metric_id in ["persistenceHF", "sciPersistenceHF", "sciPersistenceHF_protectedAreas"]) or polygon_id < 2:
-                    continue
-                else:
+        metrics = list(get_query(conn, "name", "metric", "name"))
+        polygons = list(get_query(conn, "id", "polygon", "id"))
+        log_result(f"metric encontradas: {len(metrics)}")
+        log_result(f"polygons encontrados: {len(polygons)}")
+        for metric_id in metrics:
+            # Si alguna metrica es de las siguientes se omite la consulta de polygon_metric
+            if metric_id in [
+                "persistenceHF",
+                "sciPersistenceHF",
+                "sciPersistenceHF_protectedAreas",
+            ]:
+                continue
+            else:
+                log_result(f"\n=== Metric: {metric_id} ===")
+                try:
+                    # Consulta de grupos de metricas
                     log_result(
-                        f"Consultando "
-                        f"id={polygon_id}, "
-                        f"name={metric_id}"
+                        f"Consultando grupos de la metrica {metric_id} "
                     )
-                    try:#Poblado de polygon_metric
-                        response = call_api_values(
-                            polygon_id,
-                            metric_id,
-                        )
-                        log_result(
-                            f"  Status: {response.status_code}"
-                        )
-                        if response.ok:
-                            #Si alguna metrica es de las siguientes se omite la consulta de polygon_metric_layer
-                            if metric_id in ["dpc", "statsOnSpecies", "currentHF_average", "currenrRecordsGaps_average", "timelineHF", "protectedAreas", "protectedAreas_paramo", "protectedAreas_tropicalDryForest", "protectedAreas_wetland", "protConn"]:
-                                continue
-                            else:    
-                                data = response.json()
-                                if isinstance(data, dict):
-                                    objects = [data]
-                                elif isinstance(data, list):
-                                    objects = data
-                                else:
-                                    objects = []
-                                try:
-                                    for obj in objects:
-                                        item_id = obj.get("id")
-                                        for key, value in obj.items():
-                                            if key == "id":
-                                                continue
-                                            class_id = key
-                                            if metric_id in ["recordGaps", "richness"]:
-                                                class_id = metric_id
-                                            log_result(
-                                                f"    item_id : {item_id}, "
-                                                f"class_id : {class_id}"
-                                            )
-                                            try:#Poblado de polygon_metric_layer
-                                                response = call_api_layer(
-                                                    polygon_id,
-                                                    metric_id,
-                                                    item_id,
-                                                    class_id
-                                                )
-                                                log_result(
-                                                    f"  Status: {response.status_code}"
-                                                )
-                                                if response.ok == False:
-                                                    log_result(
-                                                        f"  ERROR: {response.text}"
-                                                    )
-                                            except requests.RequestException as exc:
-                                                log_result(
-                                                    f"  ERROR de conexión: {exc}"
-                                                )
-                                except KeyError as ke:
-                                    log_result(
-                                        f"  ERROR: No hay item_id"
-                                    )
+                    response = call_api_groups(
+                        api_base=API_BASE_URL, metric_id=metric_id
+                    )
+                    log_result(f"  Status: {response.status_code}")
+                    groups = [""]
+                    if response.ok:
+                        data = response.json()
+                        if isinstance(data, list):
+                            groups = groups + data
                         else:
                             log_result(
-                                f"  ERROR: {response.text}"
+                                f"  ERROR: Respuesta inesperada: {data}"
                             )
-                    except requests.RequestException as exc:
-                        log_result(
-                            f"  ERROR de conexión: {exc}"
-                        )
+                    else:
+                        log_result(f"  Metrica sin grupo.")
+                    for group in groups:
+                        log_result(f"  group : {group}")
+                        for polygon_id in get_query(
+                            conn, "id", "polygon", "id"
+                        ):
+                            try:
+                                # Poblado de polygon_metric
+                                response = call_api_values(
+                                    api_base=API_BASE_URL,
+                                    polygon_id=polygon_id,
+                                    metric_id=metric_id,
+                                    group=group,
+                                )
+                                log_result(f"  Status: {response.status_code}")
+                                if response.ok:
+                                    # Si alguna metrica es de las siguientes se omite la consulta de polygon_metric_layer
+                                    if metric_id in [
+                                        "dpc",
+                                        "statsOnSpecies",
+                                        "currentHF_average",
+                                        "recordGaps_averages",
+                                        "timelineHF",
+                                        "protectedAreas",
+                                        "protectedAreas_paramo",
+                                        "protectedAreas_tropicalDryForest",
+                                        "protectedAreas_wetland",
+                                        "protConn",
+                                    ]:
+                                        continue
+                                    else:
+                                        data = response.json()
+                                        if isinstance(data, dict):
+                                            objects = [data]
+                                        elif isinstance(data, list):
+                                            objects = data
+                                        else:
+                                            log_result(
+                                                f"  ERROR: Respuesta inesperada: {data}"
+                                            )
+                                            continue
+                                        for obj in objects:
+                                            item_id = obj.get("id")
+                                            for key, value in obj.items():
+                                                if key == "id":
+                                                    continue
+                                                class_id = key
+                                                if metric_id in [
+                                                    "recordGaps",
+                                                    "richness",
+                                                ]:
+                                                    class_id = metric_id
+                                                log_result(
+                                                    f"    item_id : {item_id}, "
+                                                    f"class_id : {class_id}"
+                                                )
+                                                try:  # Poblado de polygon_metric_layer
+                                                    response = call_api_layer(
+                                                        api_base=API_BASE_URL,
+                                                        polygon_id=polygon_id,
+                                                        metric_id=metric_id,
+                                                        item_id=item_id,
+                                                        class_id=class_id,
+                                                        group=group,
+                                                    )
+                                                    log_result(
+                                                        f"  Status: {response.status_code}"
+                                                    )
+                                                    if response.ok == False:
+                                                        log_result(
+                                                            f"  ERROR: {response.text}"
+                                                        )
+                                                except (
+                                                    requests.RequestException
+                                                ) as exc:
+                                                    log_result(
+                                                        f"  ERROR de conexión: {exc}"
+                                                    )
+                                else:
+                                    log_result(f"  ERROR: {response.text}")
+                            except requests.RequestException as exc:
+                                log_result(f"  ERROR de conexión: {exc}")
+                except requests.RequestException as exc:
+                    log_result(f"  ERROR de conexión: {exc}")
 
 
 if __name__ == "__main__":
